7e659685c276fbfe9abba81b49074a078ba0ba6e
saisoft <saisoft@php-linux>
2015-02-25 00:38:54 -0500

    Hello World page

--- helloworld.php
+++ helloworld.php
@@ -1,8 +1,34 @@
 <?php
-
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+ 
 
+$string1 = 'Hello';
+$string2 = 'world';
+
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php
+        echo "$string1 $string2";
+        ?>
+        </title>
+    </head>
+    <body>
+        <h1 style="color:blue"><center>
+        <?php
+        echo "$string1 $string2";
+        ?>
+        </center></h1>
+        <br></br>
+        <h1 style="color:red"><center>
+        <?php
+        echo $string1." ".$string2."!"
+        ?>
+        </center></h1>
+    </body>
+</html>
